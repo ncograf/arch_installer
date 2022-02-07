447b793f2e75421bb4951b3e81e9550c9f1b8ae6
Nico Graf <ncograf@gmail.com>
2022-02-07 20:42:13 +0100

    Small fix for sound on / off

--- hardware_tools/toggle_sound.sh
+++ hardware_tools/toggle_sound.sh
@@ -2,7 +2,7 @@
 
 # toggle mute on the default sink
 user=$(id -nu $SUDO_UID)
-sudo -user $user XDG_RUNTIME_DIR=/run/user/$SUDO_UID pactl set-sink-mute @DEFAULT_SINK@ toggle
+sudo -u $user XDG_RUNTIME_DIR=/run/user/$SUDO_UID pactl set-sink-mute @DEFAULT_SINK@ toggle
 
 # chnge led ligh on keybaord
 /usr/src/hardware_tools/control_mute_led.sh
